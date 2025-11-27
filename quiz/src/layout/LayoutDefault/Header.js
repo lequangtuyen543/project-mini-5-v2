37dde1198fb0c0bd0f13d25776b174e378b55342
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-11-27 20:30:10 +0700

    4. Cập nhật header khi login

--- quiz/src/layout/LayoutDefault/Header.js
+++ quiz/src/layout/LayoutDefault/Header.js
@@ -1,28 +1,55 @@
 import { Button, Flex, Layout, Space } from 'antd';
 import './LayoutDefault.scss';
 import { NavLink } from 'react-router-dom';
+import { getCookie } from '../../helpers/cookie';
+import { useSelector } from 'react-redux';
+import { DashboardOutlined, LoginOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 export const HeaderApp = () => {
+  const token = getCookie("token");
+  const isLogin = useSelector((state) => state.loginReducer);
+
   return (
     <>
       <Header className='header'>
         <div className='container'>
           <NavLink to="/">
-            <h2>Quiz</h2>
+            <h2 className='site-name'>Quiz</h2>
           </NavLink>
 
-          <div className='nav-wrap'>
-            <NavLink to="/">Home</NavLink>
-          </div>
-
-          <Space size="middle">
-            <NavLink to="/login">
-              <Button>Login</Button></NavLink>
-            <NavLink to="/register">
-              <Button>Register</Button></NavLink>
+          <Space className='nav-wrap'>
+            <NavLink to="/">
+              <Button className='btn1'>Home</Button>
+            </NavLink>
+            {token && (
+              <>
+                <NavLink to="/topic">
+                  <Button className='btn1'>Topics</Button>
+                </NavLink>
+                <NavLink to="/answers">
+                <Button className='btn1'>Answers</Button>
+                </NavLink>
+              </>
+            )}
           </Space>
+
+          {token ? (
+            <Space size="middle">
+              <NavLink to="/dashboard">
+                <Button icon={<DashboardOutlined />}>Dashboard</Button></NavLink>
+              <NavLink to="/logout">
+                <Button icon={<LogoutOutlined />}>Logout</Button></NavLink>
+            </Space>
+          ) : (
+            <Space size="middle">
+              <NavLink to="/login">
+                <Button icon={<LoginOutlined />}>Login</Button></NavLink>
+              <NavLink to="/register">
+                <Button icon={<UserOutlined />}>Register</Button></NavLink>
+            </Space>
+          )}
         </div>
       </Header>
     </>
